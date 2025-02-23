c3ffb902543b1342969d1eb5a5ac421675fbae4e
alagozfurkan <alagozf30@gmail.com>
2025-02-23 14:26:30 +0300

    add todo via DOM to whichever project you want

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,11 +1,21 @@
 import { Project } from "./project";
 import { projectContainer } from "./projectcontainer";
+import { ToDo } from "./todo";
 
 const projectcontainer = document.querySelector(".projectcontainer");
 const todocontainerdiv = document.querySelector(".todocontainer");
 const addProject = document.querySelector(".addProject");
 const projectsubmitbutton = document.querySelector(".ProjectSubmit");
 const projectname = document.querySelector("#projectname");
+const addtodo = document.querySelector(".addTodo");
+const todosubmitbutton = document.querySelector(".todoSubmit");
+const todotitle = document.querySelector("#todotitle");
+const tododescription = document.querySelector("#tododescription");
+const tododueDate = document.querySelector("#tododueDate");
+const todofieldset = document.getElementsByName("priority");
+const todoform = document.querySelector(".todoform");
+
+
 
 function projectRenderer(projectContainer) {
     projectcontainer.textContent = "";
@@ -13,6 +23,7 @@ function projectRenderer(projectContainer) {
         const button = document.createElement("button");
         button.addEventListener("click", function() {
             todoRenderer(project.toDoContainer)
+            addTodoViaDom(project);
         })
         button.innerHTML = project.title;
         projectcontainer.appendChild(button);
@@ -31,6 +42,8 @@ projectsubmitbutton.addEventListener("click", function() {
 })
 
 
+
+
 function todoRenderer(todocontainer) {
     todocontainerdiv.textContent = "";
     todocontainer.forEach(todo => {
@@ -46,6 +59,53 @@ function addProjectViaDom() {
     console.log(projectContainer.projectContainerArray)
     projectRenderer(projectContainer.projectContainerArray);
     
+}
+
+// todosubmitbutton'a iki kere event listner ekliyor bu fonksiyounun dısına cıkması lazım. cıkarsa calısmaz bir sekilde cozmek lazım.
+//priority kısmı calısmıyor onun halledilmesi lazım
+function addTodoViaDom(projectObject) {
+    const todobutton = document.createElement("button");
+    todobutton.innerHTML = "+";
+    todocontainerdiv.appendChild(todobutton);
+
+    todobutton.addEventListener("click", function() {
+        addtodo.showModal();
+    })
+    console.log(todoform.lastElementChild.tagName)
+    if (todoform.lastElementChild.tagName == "BUTTON") {
+        todoform.lastChild.remove();
+     }
+    const todosubmitbutton = document.createElement("button");
+    todosubmitbutton.setAttribute("type", "submit")
+    todosubmitbutton.innerHTML = "Submit"
+    todoform.appendChild(todosubmitbutton);
+
+
+    todosubmitbutton.addEventListener("click", function(event) {
+        todoform.lastChild.remove()
+        event.preventDefault();
+        console.log(todoform.children)
+        
+        console.log(todoform.children)
+        projectObject.addToDo(new ToDo(todotitle.value, tododescription.value, tododueDate.value, todofieldset.value))
+        
+        todoRenderer(projectObject.toDoContainer);
+        addTodoViaDom(projectObject);
+        addtodo.close()
+        
+    })
+    
+
+
+
+    
+    
+   /*todosubmitbutton.addEventListener("click", function() {
+        projectObject.addToDo(new ToDo(todotitle.value, tododescription.value, tododueDate.value, todofieldset.value))
+        console.log(projectObject);
+        todoRenderer(projectObject.toDoContainer);
+        addTodoViaDom(projectObject);
+    }) */
 }
 
 
